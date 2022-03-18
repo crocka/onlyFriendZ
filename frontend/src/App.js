9a7ad4b3dce59de53b8ed9a810bf8c864f67f458
crocka <chino123anc@hotmail.com>
2022-03-18 18:27:06 -0400

    Implement pop up window

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import SignIn from './components/SignIn.jsx'
+import SignUp from './components/SignUp.jsx'
 import PopupWindow from './components/PopupWindow'
 import './App.css';
 function App() {
   return (
     <PopupWindow>
-    <SignIn />
+    <SignUp />
     </PopupWindow>
   );
 }
