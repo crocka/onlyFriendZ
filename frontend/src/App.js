1ce6b8cd2e6873d47b003cb2c036cfe3b6960b43
Patrick Nguyen <patar.nguyen@gmail.com>
2022-03-19 16:38:13 -0400

    Add Location Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,14 @@ import PopupWindow from './components/PopupWindow'
 import Welcome from './components/Welcome.jsx'
 import Map from './components/Map.jsx'
 import Fragment from 'react'
-
+import Location from './components/Location.jsx'
 import './App.css';
 function App() {
   return (
     <div>
       <Map></Map>
       <PopupWindow>
-        <SignUp></SignUp>
+        <Location />
       </PopupWindow>
     </div>
   );
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -11,19 +11,18 @@ export default function Map(props) {
 
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ backgroundColor:"black"}}>
+    <MapContainer center={[43.6532, -79.3832]} zoom={13} style={{ backgroundColor:"black"}}>
        
       <TileLayer
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
         url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
         // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* <Marker position={[51.505, -0.09]}>
+      {/* <Marker position={[43.6532, -79.3832]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker> */}
-      {addGeoJson()}
     </MapContainer>
   );
 }
