17b5bdf0955d7bb7f64688f87ee8c7f7a7d9be5a
crocka <chino123anc@hotmail.com>
2022-03-19 14:59:20 -0400

    Implement map

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,19 +1,29 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-export default function Map() {
+export default function Map(props) {
+
+  async function addGeoJson() {
+    const response = await fetch("https://ws.lioservices.lrc.gov.on.ca/arcgis1071a/rest/services/LIO_OPEN_DATA/LIO_Open01/MapServer/32/query?where=1%3D1&outFields=*&outSR=4326&f=json");
+    const data = await response.json();
+    return (<GeoJSON data={data}></GeoJSON>);
+}
+
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ backgroundColor:"black"}}>
        
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+        url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
+        // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {/* <Marker position={[51.505, -0.09]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker> */}
+      {addGeoJson()}
     </MapContainer>
   );
 }
