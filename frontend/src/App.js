9a3fd4fd5c0e2e405ff8813055f3f51b95702981
tayjee <jimmy.tang.360@gmail.com>
2022-03-20 14:10:38 -0400

    Add components rendering when sidebar element is clicked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,19 @@ import Welcome from './components/Welcome.jsx'
 import Map from './components/Map.jsx'
 import Sidebar from './components/Sidebar.jsx'
 import Fragment from 'react'
+import {Switch, Route} from "react-router-dom";
 
 import './App.css';
 function App() {
   return (
     <div>
       <Sidebar />
+      <Switch>
+        <Route exact from="/welcome" render={props => <PopupWindow><Welcome {...props} /></PopupWindow>} />
+        <Route exact from="/signup" render={props => <PopupWindow><SignUp {...props} /></PopupWindow>} />
+        <Route exact from="/signin" render={props => <PopupWindow><SignIn {...props} /></PopupWindow>} />
+        </Switch>
       <Map></Map>
-      <PopupWindow>
-        <SignUp></SignUp>
-      </PopupWindow>
     </div>
   );
 }
