d6431872e699be572dbd60d5b16194c772d45052
crocka <chino123anc@hotmail.com>
2022-03-21 12:52:09 -0400

    Implement location form in progress

---
+++ frontend/src/components/AddLocation/Stepper.jsx
@@ -0,0 +1,180 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import useApplicationData from '../hooks/useApplicationData';
+
+const steps = ['Add Photos and Detials', 'Choose Location', 'Confirm Submission'];
+
+export default function HorizontalLinearStepper() {
+
+  const { createUser } = useApplicationData();
+
+  const [file, setFile] = React.useState({});
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handlePage = () => {
+
+
+  };
+
+  const handleSubmit = (event) => {
+
+    //:name, :email_address, :password, :password_confirmation, :birthday, :image_url, :instagram_handle, :twitter_handle, :tiktok_handle, :personal_link, :summary
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+
+
+    const user = {
+
+      user: {
+
+        name: data.get('firstName') + " " + data.get('lastName'),
+        summary: data.get('summary'),
+        images: file.selectedFile
+
+      }
+
+    }
+
+    console.log(user)
+
+    createUser(user)
+      .then(res => {
+
+        console.log(res);
+
+      })
+      .catch(err => console.log(err))
+
+    // console.log({
+    //   name: data.get('firstName') + data.get('lastName'),
+    //   email_address: data.get('email'),
+    //   password: data.get('password'),
+    //   password_confirmation: data.get('password_confirmation'),
+    //   birthday: value,
+    //   summary: data.get('summary'),
+    //   image: file
+
+    // });
+  };
+  // });
+  const onFileChange = event => {
+
+    // Update the state
+    setFile({ selectedFile: event });
+    // console.log(event)
+
+  };
+
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          if (isStepOptional(index)) {
+            labelProps.optional = (
+              <Typography variant="caption">Optional</Typography>
+            );
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>
+            All steps completed - you&apos;re finished
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Box sx={{ flex: '1 1 auto' }} />
+            <Button onClick={handleReset}>Reset</Button>
+          </Box>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <Typography sx={{ mt: 2, mb: 1 }}>
+
+            {handlePage()}
+
+          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+            <Button
+              color="inherit"
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              sx={{ mr: 1 }}
+            >
+              Back
+            </Button>
+            <Box sx={{ flex: '1 1 auto' }} />
+            {isStepOptional(activeStep) && (
+              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                Skip
+              </Button>
+            )}
+
+            <Button onClick={handleNext}>
+              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
+          </Box>
+        </React.Fragment>
+      )}
+    </Box>
+  );
+}
