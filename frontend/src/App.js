d2d0dcd66400eef2dc39adbe01c0d78132b202b3
crocka <chino123anc@hotmail.com>
2022-03-23 23:19:19 -0400

    Meging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,14 +11,9 @@ import UserSummary from './components/Summary.jsx'
 import { Switch, Route } from "react-router-dom";
 import LocationForm from "./components/AddLocation/LocationForm.jsx"
 import LocationMarker from './components/AddLocation/LocationMarker.jsx';
-<<<<<<< HEAD
-import * as React from 'react';
-import ReviewList from './components/Profile/ReviewList';
-=======
 import { useEffect, useState } from "react";
 import Cookies from 'js-cookie'
 
->>>>>>> 981070568f2297d7d3c3f10e2b730a7b99e3fb80
 import PictureWall from './components/Profile/PictureWall';
 
 import UserProfile from './components/Profile/UserProfile';
@@ -28,16 +23,9 @@ import useApplicationData from './hooks/useApplicationData.jsx';
 import './App.css';
 function App() {
 
-<<<<<<< HEAD
-  const { getUser, state, getUserReviews } = useApplicationData();
-  const [response, setResponse] = React.useState({});
-  const [userReview, setUserReview] = React.useState([]);
-
-=======
-  const { getUser } = useApplicationData();
+  const { getUser,getUserReviews } = useApplicationData();
   const [response, setResponse] = useState({});
   const [userLogin, setUserLogin] = useState(false);
->>>>>>> 981070568f2297d7d3c3f10e2b730a7b99e3fb80
 
   // console.log(state)
 
@@ -71,16 +59,16 @@ function App() {
     checkUserLogin();
 
 
-    getUserReviews(16).then(res => {
+  //   getUserReviews(16).then(res => {
 
-      console.log(res);
-      setUserReview(res);
+  //     console.log(res);
+  //     setUserReview(res);
 
-    }).catch(err => console.error);
+  //   }).catch(err => console.error);
 
   }, [])
 
-  // console.log(response)
+  console.log(response)
 
 
 
@@ -101,12 +89,7 @@ function App() {
         {/* <LocationCard /> */}
         {/* {response.images.map((image) => {return (<img src={image} alt="" />);})} */}
         {/* <UserProfile user={response} /> */}
-<<<<<<< HEAD
-        {/* <ReviewList reviewArray={userReview} state={state} user_id={16} /> */}
-      </PopupWindow>
-=======
       {/* </PopupWindow> */}
->>>>>>> 981070568f2297d7d3c3f10e2b730a7b99e3fb80
     </div>
 
   );
